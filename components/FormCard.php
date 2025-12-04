151ea6d0b3b1282203f79fb1c00a86ef7f6c7302
mirkobechini <mirkobechini@gmail.com>
2025-12-04 12:49:47 +0100

    Feature: enhance password generation options with character type selection

--- components/FormCard.php
+++ components/FormCard.php
@@ -3,8 +3,40 @@
         <form action = "result.php" method="GET">
             <div class= "mb-3 d-flex justify-content-between">
                 <label for="password_length">Inserire la lunghezza della password</label>
-                <input type="number" id="password_length" min="0" max="50" name="password_length" class="text-end" placeholder="<?php echo $password_length; ?>">
+                <input type="number" id="password_length" min="0" max="50" name="password_length" class="text-end" required placeholder="<?php echo $password_length; ?>">
             </div>    
+            <div class= "mb-3 d-flex justify-content-between align-items-start">
+                <label for="characters_type" class="form-label">Tipologia di caratteri</label>
+                <div id="characters_type" style="width: 50%;">
+                    <div class="row row-cols-2 g-2">
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="numbers" value="1" id="numbers" checked/>
+                                <label class="form-check-label" for="numbers"> Numeri </label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="uppercase" value="1" id="uppercase" checked/>
+                                <label class="form-check-label" for="uppercase"> Lettere maiuscole </label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="lowercase" value="1" id="lowercase" checked/>
+                                <label class="form-check-label" for="lowercase"> Lettere minuscole </label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-check">
+                                <input class="form-check-input" type="checkbox" name="special" value="1" id="special" checked/>
+                                <label class="form-check-label" for="special"> Caratteri speciali </label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>   
             <button type="submit" class="btn btn-primary">
                 Generate password
             </button>
--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php 
 
-function generate_password($length){
+function generate_password(int $length, $include_numbers = true, $include_uppercase = true, $include_lowercase = true, $include_special = true):string{
     $letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
     $special_characters=['!','@','#','$','%','^','&','*','(',')','-','+','=','{','}','[',']',':',';','"',"'",'<','>','?',',','.','/','|','~','`'];
 
@@ -8,11 +8,22 @@ function generate_password($length){
 
     for($i=0; $i<$length; $i++){
         $charType = random_int(0,2);
-        if($charType === 0){
-            $password .= $letters[random_int(0, count($letters) - 1)];
-        } else if($charType === 1){
+        if($charType === 0 && ($include_lowercase || $include_uppercase)){
+            if( $include_lowercase && $include_uppercase){
+                $isUppercase = random_int(0,1);
+            } else if($include_uppercase){
+                $isUppercase = 1;
+            } else {
+                $isUppercase = 0;
+            }
+            if($isUppercase){
+                $password .= strtoupper($letters[random_int(0, count($letters) - 1)]);
+            } else {
+                $password .= $letters[random_int(0, count($letters) - 1)];
+            }            
+        } else if($charType === 1 && $include_numbers){
             $password .= random_int(0,9);
-        } else {
+        } else if($charType === 2 && $include_special){
             $password .= $special_characters[random_int(0, count($special_characters) - 1)];
         }
     }
